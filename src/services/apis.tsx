b2d7373934a59cf0a3ebd577f94e0e9dbf42a163
Jahanara Khatun <zarakhatun58@gmail.com>
2025-08-07 05:49:33 +0530

    update

--- src/services/apis.tsx
+++ src/services/apis.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//export const BASE_URL = "https://footage-flow-server.onrender.com";
-export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "https://footage-flow-server.onrender.com";
+//export const BASE_URL = "http://localhost:5000";
 
 const api = axios.create({
   baseURL: BASE_URL,
