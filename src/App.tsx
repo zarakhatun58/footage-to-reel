614cccdfd75a9fc9fc3de3f05b288368187a9dba
Jahanara Khatun <zarakhatun58@gmail.com>
2025-09-30 20:28:19 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -11,16 +11,13 @@ import { SearchInterface } from "./components/SearchInterface";
 import SavedEntries from "./components/SavedEntries";
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { AuthProvider, useAuth } from "./context/AuthContext";
-import { useEffect, useState } from "react";
 import { UploadInterface } from "./components/UploadInterface";
 import { ProjectGallery } from "./components/ProjectGallery";
 import { Navbar } from "./components/Navbar";
-import { VideoUploadArea } from "./components/VideoUploadArea";
 import { ThemeProvider } from "./components/ThemeProvider";
 import { VideoEditor } from "./components/VideoEditor";
 import Footer from "./components/Footer";
 import Gallery from "./pages/Gallery";
-import ProtectedRoute from "./context/ProtectedRoute";
 
 
 const queryClient = new QueryClient();
@@ -33,24 +30,17 @@ const AppWrapper = () => {
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/reset-password/:token" element={<ResetPassword />} />
-        {/* <Route path="/story-generator" element={<StoryGenerator />} /> */}
+        <Route path="/story-generator" element={<StoryGenerator />} />
         <Route path="/search" element={<SearchInterface />} />
         <Route path="/savedEntries" element={<SavedEntries />} />
-        {/* <Route path="/editor" element={<VideoEditor />} />
-        <Route path="/upload" element={<UploadInterface />} /> */}
-        {/* <Route path="/upload" element={<VideoUploadArea />} /> */}
-        {/* <Route path="/projects" element={<ProjectGallery />} />
-        <Route path="/gallery" element={<Gallery />} /> */}
+       <Route path="/editor" element={<VideoEditor />} />
+        <Route path="/upload" element={<UploadInterface />} /> 
+        <Route path="/projects" element={<ProjectGallery />} />
+        <Route path="/gallery" element={<Gallery />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       {/* Protected routes */}
-      <Route element={<ProtectedRoute />}>
-        <Route path="/story-generator" element={<StoryGenerator />} />
-        <Route path="/editor" element={<VideoEditor />} />
-        <Route path="/upload" element={<UploadInterface />} />
-        <Route path="/projects" element={<ProjectGallery />} />
-        <Route path="/gallery" element={<Gallery />} />
-      </Route>
+  
       <Footer />
     </BrowserRouter>
   );
